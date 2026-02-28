3c5951c036340493c6c5870351a24c9e056199ee
edevPedro <edevpedro@gmail.com>
2026-02-28 00:54:40 -0300

    Implement qbMatrix2 methods and operators

--- qbMatrix.h
+++ qbMatrix.h
@@ -1,28 +1,163 @@
-#ifndef QBMATRIX2_H 
+#ifndef QBMATRIX2_H
 #define QBMATRIX2_H
 
 template <class T>
-class qbMatrix2{
-  public:
-    //Define the various constructors
+class qbMatrix2 {
+public:
+    // Define the various constructors
     qbMatrix2();
     qbMatrix2(int nRows, int nCols);
     qbMatrix2(int nRows, int nCols, const T *inputData);
     qbMatrix2(const qbMatrix2<T>& inputMatrix);
 
-    //and the destructor
+    // Destructor
     ~qbMatrix2();
 
-    //Configuration methods
+    // Configuration methods
     bool resize(int numRows, int numCols);
 
-    //Element access methods
+    // Element access methods
     T GetElement(int row, int col);
     bool setElement(int row, int col, T elementValue);
     int getNumRows();
     int getNumCols();
 
-    //Overload == operator
-    bool operator== (const qbMatrix2<T>& rhs)
+    // Overload == operator
+    bool operator== (const qbMatrix2<T>& rhs);
 
-#endif 
+    // Overload +, -, and * operators (friends)
+    template <class U> friend qbMatrix2<U> operator+ (const qbMatrix2<U>& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator+ (const U& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator+ (const qbMatrix2<U>& lhs, const U& rhs);
+
+    template <class U> friend qbMatrix2<U> operator- (const qbMatrix2<U>& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator- (const U& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator- (const qbMatrix2<U>& lhs, const U& rhs);
+
+    template <class U> friend qbMatrix2<U> operator* (const qbMatrix2<U>& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator* (const U& lhs, const qbMatrix2<U>& rhs);
+    template <class U> friend qbMatrix2<U> operator* (const qbMatrix2<U>& lhs, const U& rhs);
+
+    private:
+      int Sub2Ind(int row, int col);
+
+    private:
+      T *m_matrixData;
+      int m_nRows, m_nCols, m_nElements;
+};
+
+//The default constructor
+template <class T>
+qbMatrix2<T>::qbMatrix2(){
+  m_nRows = 1;
+  m_nCols = 1;
+  m_nElements = 1;
+  m_matrixData = new T[m_nElements];
+  m_matrixData[0] = 0.0;
+}
+
+//Construct empty matrix (All elements 0)
+template <class T>
+qbMatrix2<T>::qbMatrix2(int nRows, int nCols){
+  m_nRows = nRows;
+  m_nCols = nCols;
+  m_nElements = m_nRows * m_nCols;
+  m_matrixData = new T[m_nElements];
+  for (int i=0; i<m_nElements; i++){
+    m_matrixData[i] = 0.0;
+  }
+
+}
+
+//Construct from const linear array.
+template <class T>
+qbMatrix2<T>::qbMatrix2(int nRows, int nCols, const T *inputData){
+  m_nRows = nRows;
+  m_nCols = nCols;
+  m_nElements = m_nRows * m_nCols;
+  m_matrixData = new T[m_nElements];
+  for (int i=0; i<m_nElements; i++){
+    m_matrixData[i] = inputData[i];
+  }
+}
+
+//Copy constructor
+template <class T>
+qbMatrix2<T>::qbMatrix2(const qbMatrix2<T>& inputMatrix){
+  m_nRows = inputMatrix.m_nRows;
+  m_nCols = inputMatrix.m_nCols;
+  m_nElements = inputMatrix.m_nElements;
+  m_matrixData = new T[m_nElements];
+  for (int i=0; i<m_nElements; i++){
+    m_matrixData[i] = inputMatrix.m_matrixData[i];
+  }
+}
+
+//Destructor
+template <class T>
+qbMatrix2<T>::~qbMatrix2(){
+  if (m_matrixData != nullptr){
+    delete [] m_matrixData;
+  }
+}
+
+/*********************************
+CONFIGURATION FUNCTIONS
+**********************************/
+template <class T>
+bool qbMatrix2<T>::resize(int numRows, int numCols){
+  m_nRows = numRows;
+  m_nCols = numCols;
+  m_nElements = m_nRows * m_nCols;
+  delete [] m_matrixData;
+  m_matrixData = new T[m_nElements];
+  if (m_matrixData != nullptr){
+    for (int i=0; i<m_nElements; i++){
+      m_matrixData[i] = 0.0;
+    }
+    return true;
+  }
+    return false;
+  }
+
+
+  /*********************************
+   ELEMENT FUNCTIONS
+  **********************************/
+
+  template <class T>
+  T qbMatrix2<T>::GetElement(int row, int col){
+  int linearIndex = Sub2Ind(row, col);
+  if (linearIndex >= 0){
+    return m_matrixData[linearIndex];
+  }
+  return 0.0;
+}
+
+template <class T>
+bool qbMatrix2<T>::setElement(int row, int col, T elementValue){
+  int linearIndex = Sub2Ind(row, col);
+  if (linearIndex >= 0){
+    m_matrixData[linearIndex] = elementValue;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+int qbMatrix2<T>::getNumRows(){
+  return m_nRows;
+}
+
+template <class T>
+int qbMatrix2<T>::getNumCols(){
+  return m_nCols;
+}
+
+
+
+
+
+
+
+#endif
